57da43941107f21210b1fcd559c1d5579aa1570e
Phil Whelan <phil123@gmail.com>
2013-05-20 10:29:13 -0700

    updated for use with glu-mq proxy

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+
 var httpProxy = require('http-proxy');
 
-var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+var rabbitmq_service_name = process.env.RABBITMQ_SERVICE_NAME
+var stackato_services = JSON.parse(process.env.STACKATO_SERVICES);
+var rabbitmq_settings = stackato_services[rabbitmq_service_name];
 
-var host = vcap_services.rabbitmq[0].credentials.host;
-var port = parseInt(vcap_services.rabbitmq[0].credentials.port) + 10000;
+var service_host = rabbitmq_settings.host;
+var service_port = parseInt(rabbitmq_settings.port) + 10000;
+var listen_port = process.env.PORT;
 
-httpProxy.createServer(port, host).listen(process.env.PORT);
+console.log("rabbitmq_settings: ", rabbitmq_settings);
+httpProxy.createServer(service_port, service_host).listen(listen_port);
+
